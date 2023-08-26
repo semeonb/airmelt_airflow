840e585c88b74cee0d2ad6a3e6bc6f9e83f705b8
bsemeon <bsemeon@gmail.com>
2023-08-25 20:35:34 -0400

    update dep

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     description="Airflow Utilities",
     packages=["airmelt_airflow_operators"],
     install_requires=[
+        "mysqlclient==2.2.0",
         "apache-airflow==2.7.0",
         "apache-airflow-providers-common-sql==1.7.0",
         "apache-airflow-providers-ftp==3.5.0",
@@ -16,5 +17,6 @@ setuptools.setup(
         "apache-airflow-providers-imap==3.3.0",
         "apache-airflow-providers-microsoft-mssql==3.4.2",
         "apache-airflow-providers-sqlite==3.4.3",
+        "apache-airflow-providers-mysql==5.2.1"
     ],
 )
