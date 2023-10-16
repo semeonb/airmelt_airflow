e673bf7f6e8589cf4276eafd2d6ca5f0da098033
bsemeon <bsemeon@gmail.com>
2023-10-16 16:28:21 -0400

    add dbt

--- airmelt_airflow_operators/dbt.py
+++ airmelt_airflow_operators/dbt.py
@@ -15,58 +15,76 @@ class AirflowDbtTaskGroup(DbtTaskGroup):
     The AirflowDbtTaskGroup is a subclass of the DbtTaskGroup that is used to run dbt models in Airflow.
 
     Parameters
-    __________
-
-    group_id: str name of the task group
-    dbt_project_path: PathLike path to the dbt project folder, for example {os.environ['AIRFLOW_HOME']}/dags/dbt/{profile_name}
-    dbt_model_path: PathLike path to the dbt model folder, for example "{os.environ['AIRFLOW_HOME']}/dags/dbt/codere_core/models/{DBT_MODEL}"
-    dbt_profile_name: str name of the dbt profile
-    target_name: str name of the dbt target, usually the environment name (dev, prod, etc.)
-    connection_id: str name of the Airflow connection that contains the GCP credentials
-    project_id: str name of the GCP project
-    dataset: str name of the BigQuery target dataset
-    keyfile: str path to the GCP service account key file
-    location: str location of the BigQuery dataset, default is "US"
-    method: str method of authentication, default is "service-account", other options are "oauth" and "application-default"
+    ----------
+    group_id : str, optional
+        Name of the task group, by default "dbt_task_group"
+    dbt_project_path : PathLike, optional
+        Path to the dbt project folder, for example {os.environ['AIRFLOW_HOME']}/dags/dbt/{profile_name}
+    dbt_model_path : PathLike, optional
+        Path to the dbt model folder, for example "{os.environ['AIRFLOW_HOME']}/dags/dbt/codere_core/models/{DBT_MODEL}"
+    dbt_profile_name : str, optional
+        Name of the dbt profile
+    target_name : str, optional
+        Name of the dbt target, usually the environment name (dev, prod, etc.)
+    connection_id : str, optional
+        Name of the Airflow connection that contains the GCP credentials
+    project_id : str, optional
+        Name of the GCP project
+    dataset : str, optional
+        Name of the BigQuery target dataset
+    keyfile : str, optional
+        Path to the GCP service account key file
+    location : str, optional
+        Location of the BigQuery dataset, default is "US"
+    method : str, optional
+        Method of authentication, default is "service-account", other options are "oauth" and "application-default"
+    threads : int, optional
+        Number of threads to use for dbt execution, default is 1
+    dbt_executable_path : PathLike, optional
+        Path to the dbt executable, for example {os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt
     """
 
     def __init__(
         self,
-        group_id: str = "dbt_task_group",
-        dbt_project_path: PathLike = None,
-        dbt_model_path: PathLike = None,
-        dbt_profile_name: str = None,
-        target_name: str = None,
-        connection_id: str = None,
-        project_id: str = None,
-        dataset: str = None,
-        keyfile: str = None,
-        location: str = "US",
-        method: str = "service-account",
-        *args: Any,
-        **kwargs: Any
-    ) -> None:
-        super().__init__(group_id, *args, **kwargs)
-        profile_mapping = GoogleCloudOauthProfileMapping(
-            conn_id=connection_id,
-            profile_args={
-                "project": project_id,
-                "dataset": dataset,
-                "location": location,
-                "method": method,
-                "keyfile": keyfile,
-            },
+        group_id="dbt_task_group",
+        dbt_project_path=None,
+        dbt_model_path=None,
+        dbt_profile_name=None,
+        target_name=None,
+        connection_id=None,
+        project_id=None,
+        dataset=None,
+        keyfile=None,
+        location="US",
+        method="service-account",
+        threads=1,
+        dbt_executable_path=None,
+        *args,
+        **kwargs
+    ):
+        super().__init__(
+            group_id,
+            profile_config=ProfileConfig(
+                profile_name=dbt_profile_name,
+                target_name=target_name,
+                profile_mapping=GoogleCloudOauthProfileMapping(
+                    conn_id=connection_id,
+                    profile_args={
+                        "project": project_id,
+                        "dataset": dataset,
+                        "location": location,
+                        "method": method,
+                        "keyfile": keyfile,
+                        "threads": threads,
+                    },
+                ),
+            ),
+            project_config=ProjectConfig(dbt_project_path),
+            execution_config=ExecutionConfig(dbt_executable_path=dbt_executable_path),
+            render_config=RenderConfig(select=["path:{}".format(dbt_model_path)]),
+            *args,
+            **kwargs
         )
-        profile_config = ProfileConfig(
-            profile_name=dbt_profile_name,
-            target_name=target_name,
-            profile_mapping=profile_mapping,
-        )
-        execution_config = ExecutionConfig(threads=1)
-        self.project_config = ProjectConfig(dbt_project_path)
-        self.profile_config = profile_config
-        self.execution_config = execution_config
-        self.render_config = RenderConfig(select=["path:{}".format(dbt_model_path)])
 
 
 # DbtTaskGroup(
