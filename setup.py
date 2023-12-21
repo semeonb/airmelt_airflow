fb9938c898ddf889cfc60e5b00d135c1494f2277
Semeon Balagula <semeonbalagula@semeons-mbp.mynetworksettings.com>
2023-12-20 21:34:21 -0500

    update deps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="airmelt_airflow",
-    version="0.0.5",
+    version="0.0.6",
     author="Semeon Balagula @ Airmelt",
     description="Airflow Utilities",
     packages=["airmelt_airflow_operators"],
