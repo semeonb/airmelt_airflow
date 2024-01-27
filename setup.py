b883f40413805ecf9e923d1fb9c4730ab3cc7e0a
Semeon Balagula <semeonbalagula@semeons-mbp.mynetworksettings.com>
2024-01-27 12:00:14 -0500

    add gc module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="airmelt_airflow",
-    version="0.1.0",
+    version="0.1.1",
     author="Semeon Balagula @ Airmelt",
     description="Airflow Utilities",
     packages=["airmelt_airflow_operators"],
