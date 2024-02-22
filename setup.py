31a41477587bf3cfe8036d89648bf0abf7e413d1
Semeon Balagula <semeonbalagula@semeons-mbp.mynetworksettings.com>
2024-02-22 10:17:26 -0500

    upgrade tpo apache-airflow 2.8.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="airmelt_airflow",
-    version="0.2.2",
+    version="0.2.3",
     author="Semeon Balagula @ Airmelt",
     description="Airflow Utilities",
     packages=["airmelt_airflow_operators"],
