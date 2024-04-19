5adbdb63ee1a5a7f94398a6dffe096eaa80476a0
Semeon Balagula <semeonbalagula@semeons-mbp.mynetworksettings.com>
2024-04-19 16:29:06 -0400

    update bqhook

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="airmelt_airflow",
-    version="0.4.4",
+    version="0.4.5",
     author="Semeon Balagula @ Airmelt",
     description="Airflow Utilities",
     packages=["airmelt_airflow_operators"],
